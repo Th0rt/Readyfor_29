86e7ec76f9b2a55912db9f0b78f41e2900052822
関谷佑斗 <yhk3165t@gmail.com>
2018-07-04 15:21:05 +0900

    ルーティングの編集(users/show,editとdevise for:users)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :image_upload_tests
+  resources :users, only: [:show, :edit]
   root to: 'toppage#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
