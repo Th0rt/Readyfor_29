12308a98f7f6f639d43cf10f92ad07e21670f645
関谷佑斗 <yhk3165t@gmail.com>
2018-07-07 00:03:19 +0900

    users/edit.html.hamlのJSの関数化

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -26,123 +26,34 @@ $(function(){
   })
 });
 
-// 名前(ニックネーム)編集ボタン
+//関数化
 $(function(){
-  $("#nickname-edit-button").click(function(){
-    $("#user-nickname-explain").toggleClass('hidden');
-    $("#user-edit-nickname").toggleClass('hidden');
-    var result = $("#nickname-edit-button")[0].innerText
-    if (result  === "編集" ){
-      $("#nickname-edit-button").text('キャンセル')
-    }
-    else{
-      $("#nickname-edit-button").text("編集")
-    }
-  })
+  function editButton(button, explain, edit){
+    $(button).click(function(){
+      $(explain).toggleClass('hidden');
+      $(edit).toggleClass('hidden');
+      var result = $(button)[0].innerText
+      if ( result === '編集' ){
+        $(button).text('キャンセル')
+      }else{
+        $(button).text('編集')
+      }
+    })
+  }
+  // 名前(ニックネーム)編集ボタン
+  editButton("#nickname-edit-button", "#user-nickname-explain", "#user-edit-nickname");
+  // メールアドレス編集ボタン
+  editButton("#email-edit-button", "#user-email-explain","#user-edit-email");
+  //パスワード編集ボタン
+  editButton("#password-edit-button","#user-password-explain","#user-edit-password");
+  //プロフィール編集ボタン
+  editButton("#content-edit-button","#user-content-explain","#user-edit-content");
+  //プロフィール画像編集ボタン
+  editButton("#image-edit-button","#user-image-explain","#user-edit-image");
+  //表示するWebサイト
+  editButton("#website-edit-button","#website-image-explain","#user-edit-website");
+  //支援情報の公開
+  editButton("#return-edit-button","#user-return-explain","#user-edit-return");
+  editButton("#alertmail-edit-button","#user-alertmail-explain","#user-edit-alertmail");
 })
 
-// メールアドレス編集ボタン
-$(function(){
-  $("#email-edit-button").click(function(){
-    $("#user-email-explain").toggleClass('hidden');
-    $("#user-edit-email").toggleClass('hidden');
-    var result = $("#email-edit-button")[0].innerText;
-    if (result  === "編集" ){
-      $("#email-edit-button").text('キャンセル')
-    }
-    else{
-      $("#email-edit-button").text("編集")
-    }
-  })
-})
-
-//パスワード編集ボタン
-$(function(){
-  $("#password-edit-button").click(function(){
-    $("#user-password-explain").toggleClass('hidden');
-    $("#user-edit-password").toggleClass('hidden');
-    var result = $("#password-edit-button")[0].innerText
-    if (result  === "編集" ){
-      $("#password-edit-button").text('キャンセル')
-    }
-    else{
-      $("#password-edit-button").text("編集")
-    }
-  })
-})
-
-//プロフィール編集ボタン
-$(function(){
-  $("#content-edit-button").click(function(){
-    $("#user-content-explain").toggleClass('hidden');
-    $("#user-edit-content").toggleClass('hidden');
-    var result = $("#content-edit-button")[0].innerText
-    if (result  === "編集" ){
-      $("#content-edit-button").text('キャンセル')
-    }
-    else{
-      $("#content-edit-button").text("編集")
-    }
-  })
-})
-
-
-//プロフィール画像編集ボタン
-$(function(){
-  $("#image-edit-button").click(function(){
-    $("#user-image-explain").toggleClass('hidden');
-    $("#user-edit-image").toggleClass('hidden');
-    var result = $("#image-edit-button")[0].innerText
-    if (result  === "編集" ){
-      $("#image-edit-button").text('キャンセル')
-    }
-    else{
-      $("#image-edit-button").text("編集")
-    }
-  })
-})
-
-//表示するWebサイト
-$(function(){
-  $("#website-edit-button").click(function(){
-    $("#website-image-explain").toggleClass('hidden');
-    $("#user-edit-website").toggleClass('hidden');
-    var result = $("#website-edit-button")[0].innerText
-    if (result  === "編集" ){
-      $("#website-edit-button").text('キャンセル')
-    }
-    else{
-      $("#website-edit-button").text("編集")
-    }
-  })
-})
-
-//支援情報の公開
-$(function(){
-  $("#return-edit-button").click(function(){
-    $("#user-return-explain").toggleClass('hidden');
-    $("#user-edit-return").toggleClass('hidden');
-    var result = $("#return-edit-button")[0].innerText
-    if (result  === "編集" ){
-      $("#return-edit-button").text('キャンセル')
-    }
-    else{
-      $("#return-edit-button").text("編集")
-    }
-  })
-})
-
-//メール通知設定
-$(function(){
-  $("#alertmail-edit-button").click(function(){
-    $("#user-alertmail-explain").toggleClass('hidden');
-    $("#user-edit-alertmail").toggleClass('hidden');
-    var result = $("#alertmail-edit-button")[0].innerText
-    if (result  === "編集" ){
-      $("#alertmail-edit-button").text('キャンセル')
-    }
-    else{
-      $("#alertmail-edit-button").text("編集")
-    }
-  })
-})
