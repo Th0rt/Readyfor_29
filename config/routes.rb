586630ef4c6c1a94d5c8a55c3f5245fcc665964f
関谷佑斗 <yhk3165t@gmail.com>
2018-07-07 13:16:30 +0900

    ルーティングにprojectsを加筆

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   root to: 'toppage#index'
   devise_for :users
-  
+
   resources :image_upload_tests
-  
+
   resources :users, only: [:show, :edit, :index] do
     resources :messages, only: [:index, :new]
   end
+  resources :projects, only: [:index, :show]
 end
