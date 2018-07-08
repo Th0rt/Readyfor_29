19e79dad57005cc0e367e6dee3747e2bf3a2eec3
kanji <nagao@nagaokaBookpuro.lan>
2018-07-08 20:37:22 +0900

    tab画面切り替え

--- app/assets/javascripts/projects/new-tabpage.js
+++ app/assets/javascripts/projects/new-tabpage.js
@@ -1,11 +1,22 @@
 $(function () {
-  //タブクリック時の処理
-  $('#new-project .col-sm-2 .nav-sidebarul .nav.tabs li').click(function () {
-    //.index()を使いクリックされたタブの順番を変数indexに代入する
-    var index = $('#new-project .col-sm-2 .nav-sidebarul .nav.tabs li').index(this);
-    //指定した全コンテンツを非表示にする
-    $('.project-tabpage').css('display', 'none');
-    //クリックされたタブと同じ順番のコンテンツのみを表示させる
-    $('.project-tabpage').eq(index).css('display', 'block');
+  //クリックしたときのファンクションをまとめて指定
+  $('ul.tabs li').click(function () {
+    //.index()を使いクリックされたタブが何番目かを調べ、
+    //	indexという変数に代入します。
+    var index = $('.tabs li').index(this);
+
+    $('.tab-content').removeClass('hide');
+
+    //コンテンツを一度すべて非表示にし、
+    $('.tab-content').css('display', 'none');
+
+    //クリックされたタブと同じ順番のコンテンツを表示します。
+    $('.tab-content').eq(index).fadeIn();
+
+    //タブについているクラスselectを消し、
+    $('.tabs li').removeClass('select');
+
+    //クリックされたタブのみにクラスselectをつけます。
+    $(this).addClass('select')
   });
 });
