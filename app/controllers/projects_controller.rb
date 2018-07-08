9713495bc021a4657afbd4c3c23e81f84a35fce4
kanji <nagao@nagaokaBookpuro.lan>
2018-07-09 00:09:59 +0900

    projectのparams作成

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,8 +6,27 @@ class ProjectsController < ApplicationController
   end
 
   def new
+    @project = Project.new
   end
 
   def create
+    @project = Prototype.new(project_params)
+    if @project.save
+
+    else
+      render action: :new
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(
+      :title,
+      :content,
+      :limit_date,
+      :goal,
+      :next_goal
+    )
   end
 end
