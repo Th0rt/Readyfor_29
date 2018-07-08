a4dd49dccb63240e6c03be34cf3e94f7a50c6a0b
kanji <nagao@nagaokaBookpuro.lan>
2018-07-08 21:06:25 +0900

    コメント削除

--- app/assets/javascripts/projects/new-tabpage.js
+++ app/assets/javascripts/projects/new-tabpage.js
@@ -1,22 +1,10 @@
 $(function () {
-  //クリックしたときのファンクションをまとめて指定
   $('ul.tabs li').click(function () {
-    //.index()を使いクリックされたタブが何番目かを調べ、
-    //	indexという変数に代入します。
     var index = $('.tabs li').index(this);
-
     $('.tab-content').removeClass('hide');
-
-    //コンテンツを一度すべて非表示にし、
     $('.tab-content').css('display', 'none');
-
-    //クリックされたタブと同じ順番のコンテンツを表示します。
     $('.tab-content').eq(index).fadeIn();
-
-    //タブについているクラスselectを消し、
     $('.tabs li').removeClass('select');
-
-    //クリックされたタブのみにクラスselectをつけます。
     $(this).addClass('select')
   });
 });
