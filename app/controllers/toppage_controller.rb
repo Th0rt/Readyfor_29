53488176b46471b92a4401445d41a0199b5ef844
Th0rt <yamaxanado@gmail.com>
2018-07-09 16:50:23 +0900

    [Add] Make page file, link, route

--- app/controllers/toppage_controller.rb
+++ app/controllers/toppage_controller.rb
@@ -1,4 +1,19 @@
 class ToppageController < ApplicationController
   def index
   end
+
+  def socialgood
+  end
+
+  def local
+  end
+
+  def product
+  end
+
+  def art
+  end
+
+  def challenge
+  end
 end
