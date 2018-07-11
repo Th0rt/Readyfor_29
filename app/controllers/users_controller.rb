47b65cd8551746442ba61ba4d245bb9fcd559676
関谷佑斗 <yhk3165t@gmail.com>
2018-07-11 14:49:15 +0900

    コントローラの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,14 @@ class UsersController < ApplicationController
   def edit
   end
 
+  def update
+    current_user.update(user_params)
+    redirect_to edit_user_path(current_user)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:nickname, :email)
+  end
+
 end
