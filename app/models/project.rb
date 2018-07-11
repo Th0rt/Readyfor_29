0bf3c4fdfd9e6500c88d6f6c7b1adc6fa9ec3ad9
kanji <nagao@nagaokanjinoMacBook-puro.local>
2018-07-11 12:56:18 +0900

    募集期間の残り日数

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,14 @@
 class Project < ApplicationRecord
+
+  # 募集期間内(残り日数を返す)、募集期間終了(終了日を返す)
+  def days_left
+    limit_date = self.limit_date
+    days_left_date = limit_date - Date.today
+    days_left = days_left_date.numerator
+    if days_left >= 0
+      "#{days_left}日"
+    else
+      limit_date.to_s(:month_day)
+    end
+  end
 end
