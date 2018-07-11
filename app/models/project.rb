645e392d7a0881880bbca9020a9eefdd686d53c0
kanji <nagao@nagaokanjinoMacBook-puro.local>
2018-07-11 15:46:29 +0900

    残り日数算出修正

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,12 +1,12 @@
 class Project < ApplicationRecord
 
   # 募集期間内(残り日数を返す)、募集期間終了(終了日を返す)
-  def days_left
+  def days_life
     limit_date = self.limit_date
-    days_left_date = limit_date - Date.today
-    days_left = days_left_date.numerator
-    if days_left >= 0
-      "#{days_left}日"
+    days_life_date = limit_date - DateTime.now
+    days_life = (days_life_date / 24 / 60 / 60).to_i
+    if days_life >= 0
+      "#{days_life}日"
     else
       limit_date.to_s(:month_day)
     end
--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,2 +1,2 @@
-Date::DATE_FORMATS[:default] = '%Y/%m/%d'
-Date::DATE_FORMATS[:month_day] = '%m/%d'
+Time::DATE_FORMATS[:default] = '%Y/%m/%d'
+Time::DATE_FORMATS[:month_day] = '%m/%d'
