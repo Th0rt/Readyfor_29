e0dead43d74e6ed1ebe672ce7f77f5e01f0387d5
kanji <nagao@nagaokanjinoMacBook-puro.local>
2018-07-12 15:42:43 +0900

    メソッドの細分化

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,14 +1,19 @@
 class Project < ApplicationRecord
 
-  # 募集期間内(残り日数を返す)、募集期間終了(終了日を返す)
-  def days_life
-    limit_date = self.limit_date
+  # 募集中かどうかを判定
+  def active?
+    @limit_date = self.limit_date
     days_life_date = limit_date - Time.current
-    days_life = (days_life_date / 24 / 60 / 60).to_i
-    if days_life >= 0
-      "#{days_life}日"
+    @days_life = (days_life_date / 24 / 60 / 60).to_i
+    @days_life >= 0
+  end
+
+  # 終了日をStringで返す
+  def limit_date_to_month_day
+    if active?
+      "#{@days_life}日"
     else
-      limit_date.to_s(:month_day)
+      @limit_date.to_s(:month_day)
     end
   end
 end
