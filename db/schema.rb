933f3a3e68d3185c461f7040a0c11366b9bf3090
関谷佑斗 <yhk3165t@gmail.com>
2018-07-12 11:01:19 +0900

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_08_135548) do
+ActiveRecord::Schema.define(version: 2018_07_12_014317) do
 
   create_table "image_upload_tests", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "image"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2018_07_08_135548) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "nickname"
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
