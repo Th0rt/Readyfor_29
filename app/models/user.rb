57d79ac143bf18ec63a5ed0ef489e25d2339a3ec
kanji <nagao@nagaokanjinoMacBook-puro.local>
2018-07-12 11:35:05 +0900

    アソシエーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :nickname, presence: true, length: { maximum: 10 }
+
+  has_many :projects
 end
