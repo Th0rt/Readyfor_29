cc94a0202f67d38b5d4b7c7648a8e0eebf65eeb6
Saoki11111 <t.aoki11111@gmail.com>
2018-07-13 18:46:09 +0900

    [add]Facebook認証機能のファイルを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,25 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
- def self.find_for_oauth(auth)
-   user = User.where(uid: auth.uid, provider: auth.provider).first
-
-   unless user
-     user = User.create(
-       uid:      auth.uid,
-       provider: auth.provider,
-       email:    User.dummy_email(auth),
-       password: Devise.friendly_token[0, 20]
-     )
-   end
-
-   user
- end
-
- private
-
- def self.dummy_email(auth)
-   "#{auth.uid}-#{auth.provider}@example.com"
- end
-
+  def self.from_omniauth(auth)
+     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+       user.email = auth.info.email
+       user.password = Devise.friendly_token[0,20]
+       # user.name = auth.info.name   # assuming the user model has a name
+       # user.image = auth.info.image # assuming the user model has an image
+     end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,11 @@
 Rails.application.routes.draw do
   root to: 'toppage#index'
-  devise_for :users
-
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :image_upload_tests
 
   resources :users, only: [:show, :edit, :index] do
     resources :messages, only: [:index, :new]
   end
   resources :projects, only: [:index, :show, :new, :create]
-
-  # コールバック用URL
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-
-  # ログアウト用
-  devise_scope :user do
-    delete :sign_out, to: 'devise/sessions#destroy', as: :destroy_user_session
-  end
 
 end
