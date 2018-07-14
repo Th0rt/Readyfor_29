ec3a416c4fc3066c68df200e6b0e288e6967973f
関谷佑斗 <yhk3165t@gmail.com>
2018-07-14 17:12:43 +0900

    js一部記載

---
+++ app/assets/javascripts/projects/search.js
@@ -0,0 +1,12 @@
+$(document).on('turbolinks:load', function(){
+  function changecolor(id){
+    $(id).hover(function(){
+      $(this).css('color', '#FFA500' )
+    },function(){
+      $(this).css('color' , '' )
+    });
+  }
+  changecolor('#search-project')
+  changecolor('#search-category')
+  changecolor('#search-area')
+});
