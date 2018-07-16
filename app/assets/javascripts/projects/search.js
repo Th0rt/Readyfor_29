ffe7a4d9349449b1de4499445ee1d1b5df7722ae
関谷佑斗 <yhk3165t@gmail.com>
2018-07-16 14:18:11 +0900

    修正

--- app/assets/javascripts/projects/search.js
+++ app/assets/javascripts/projects/search.js
@@ -1,26 +1,23 @@
-$(document).on('turbolinks:load', function(){
+$(document).on('turbolinks:load',function(){
   function changecolor(id){
     $(id).hover(function(){
-      $(this).css('color', '#FFA500')
+      $(this).css('color','#FFA500')
     },function(){
-      $(this).css('color' , '')
+      $(this).css('color','')
     });
   }
-
   function change_background(item){
     $(item).hover(function(){
       $(this).css('background-color', '#FFA500')
       $(this).find('a').css('color', 'white')
       $(this).find('i').css('color', 'white')
     },function(){
-      $(this).css('background-color' , '')
-      $(this).find('a').css('color', '')
-      $(this).find('i').css('color', '')
+      $(this).css('background-color','')
+      $(this).find('a').css('color','')
+      $(this).find('i').css('color','')
     });
   }
   changecolor('#search-category')
   changecolor('#search-area')
-  change_background(".nav-menu-item")
+  change_background('.nav-menu-item')
 });
-
-
