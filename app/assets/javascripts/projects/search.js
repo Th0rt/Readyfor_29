7f03a20aa10edfe93d69d092c8d8ffb87d182176
関谷佑斗 <yhk3165t@gmail.com>
2018-07-16 12:16:12 +0900

    背景の色変更も対応

--- app/assets/javascripts/projects/search.js
+++ app/assets/javascripts/projects/search.js
@@ -6,7 +6,21 @@ $(document).on('turbolinks:load', function(){
       $(this).css('color' , '')
     });
   }
-  changecolor('#search-project')
+
+  function change_background(item){
+    $(item).hover(function(){
+      $(this).css('background-color', '#FFA500')
+      $(this).find('a').css('color', 'white')
+      $(this).find('i').css('color', 'white')
+    },function(){
+      $(this).css('background-color' , '')
+      $(this).find('a').css('color', '')
+      $(this).find('i').css('color', '')
+    });
+  }
   changecolor('#search-category')
   changecolor('#search-area')
+  change_background(".nav-menu-item")
 });
+
+
