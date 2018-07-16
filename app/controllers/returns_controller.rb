a3825ef7df180e98b88f699343d51a3ad80c2d05
関谷佑斗 <yhk3165t@gmail.com>
2018-07-16 18:58:30 +0900

    コントローラの編集

--- app/controllers/returns_controller.rb
+++ app/controllers/returns_controller.rb
@@ -1,2 +1,16 @@
 class ReturnsController < ApplicationController
+  def new
+    @project = Project.find(params[:project_id])
+    @return = Return.new
+  end
+
+  def create
+    Return.create(return_params)
+    redirect_to root_path
+  end
+
+  private
+  def return_params
+    params.require(:return).permit(:title, :price, :stock, :content, :arrival_date, :returnimage).merge(project_id: params[:project_id])
+  end
 end
