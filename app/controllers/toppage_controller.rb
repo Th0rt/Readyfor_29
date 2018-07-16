010d711bb70a7d5bcf1c8eec485b6754ef309035
Th0rt <yamaxanado@gmail.com>
2018-07-16 14:01:22 +0900

    別ブランチの変更を削除

--- app/controllers/toppage_controller.rb
+++ app/controllers/toppage_controller.rb
@@ -4,10 +4,6 @@ class ToppageController < ApplicationController
     view_history = cookie_find_or_create("project_view_history")
     @recent_viewed_projects = Project.where(id: view_history)
                                      .order(['field(id, ?)', view_history])
-
-    @large_amount_projects = @projects.sort_by{ |project| project.total_support }
-                                      .reverse
-                                      .first(4)
   end
 
   def socialgood
