d77ec8bef8f62c0d168b23df9430d9c364c4b9c7
tsukasa yamada <yamaxanado@gmail.com>
2018-07-17 14:57:21 +0900

    メソッドのくくりだし

--- app/controllers/toppage_controller.rb
+++ app/controllers/toppage_controller.rb
@@ -1,19 +1,13 @@
 class ToppageController < ApplicationController
+  before_action :set_projects_all,
+                :set_projects_nortable,
+                :set_projects_large_amount,
+                :set_projects_new
+
   def index
-    # NOTE: 各欄ごとにクエリを発行したくないので、allで一括取得して加工する。
-    @projects = Project.all
-
-    # NOTE: 全プロジェクトを渡しているのがパフォーマンス的に良くないと思うので、あとで修正したい。
-    @new_project = @projects.sort_by{ |project| project.created_at }
-                            .reverse
-
     view_history = cookie_find_or_create("project_view_history")
     @recent_viewed_projects = Project.where(id: view_history)
                                      .order(['field(id, ?)', view_history])
-
-    @projects_large_amount = @projects.sort_by{ |project| project.total_support }
-                                      .reverse
-                                      .first(4)
   end
 
   def socialgood
@@ -29,5 +23,27 @@ class ToppageController < ApplicationController
   end
 
   def challenge
+  end
+
+  private
+
+  def set_projects_all
+    @projects = Project.all
+  end
+
+  def set_projects_new
+    # NOTE: 全プロジェクトを渡しているのがパフォーマンス的に良くないと思うので、あとで修正したい。
+    @projects_new = @projects.sort_by{ |project| project.created_at }
+                             .reverse
+  end
+
+  def set_projects_nortable
+    @projects_nortable = @projects
+  end
+
+  def set_projects_large_amount
+    @projects_large_amount = @projects.sort_by{ |project| project.total_support }
+                                      .reverse
+                                      .first(4)
   end
 end
