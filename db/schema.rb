809aebcbd51198f087fce8e99b2366e19b1b5980
関谷佑斗 <yhk3165t@gmail.com>
2018-07-17 17:51:54 +0900

    defaultで空白の追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_14_070139) do
+ActiveRecord::Schema.define(version: 2018_07_17_084437) do
 
   create_table "image_upload_tests", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "image"
@@ -51,7 +51,7 @@ ActiveRecord::Schema.define(version: 2018_07_14_070139) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "nickname"
-    t.string "avatar"
+    t.string "avatar", default: ""
     t.string "profile"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
