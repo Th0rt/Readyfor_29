8abf1dc3d69598c055d6fcfb86bbb2c6d74bb89b
tsukasa yamada <yamaxanado@gmail.com>
2018-07-17 18:03:35 +0900

    不要なメソッドを削除

--- app/models/project.rb
+++ app/models/project.rb
@@ -10,19 +10,6 @@ class Project < ApplicationRecord
     @days_life >= 0
   end
 
-  # 終了日をStringで返す
-  def limit_date_to_month_day
-    if active?
-      "#{@days_life}日"
-    else
-      @limit_date.to_s(:month_day)
-    end
-  end
-
-  def remaining_funding_days
-    ( self.limit_date.to_date - Time.current.to_date ).to_i
-  end
-
   def achievement_rate
     return 0 if self.total_support == 0
     ((self.total_support.to_f / self.goal.to_f) * 100).floor
