2280a2a480b966fb8a8af64ade2a2383bb0bdad5
関谷佑斗 <yhk3165t@gmail.com>
2018-07-17 21:15:10 +0900

    リターンを購入するボタンのjs(細かいけど)

---
+++ app/assets/javascripts/projects/project-return.js
@@ -0,0 +1,11 @@
+$(document).on('turbolinks:load',function(){
+  function change_button(button){
+    $(button).hover(function(){
+      console.log(this)
+      $(this).css('box-shadow', 'none')
+    },function(){
+      $(this).css('box-shadow', '0 2px rgba(0,0,0,0.2)')
+    });
+  }
+  change_button('.project-return__purchase .project-select-button')
+});
