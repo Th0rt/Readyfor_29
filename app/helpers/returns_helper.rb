4753202e12a738b6adefe859754b350d0166bf14
kanji <nagao@nagaokanjinoMacBook-puro.local>
2018-07-19 15:48:34 +0900

    Sider修正

--- app/helpers/returns_helper.rb
+++ app/helpers/returns_helper.rb
@@ -1,4 +1,6 @@
 module ReturnsHelper
+
+  # optionのdata
   def return_count_options
     ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
      "11", "12", "13", "14", "15", "16", "17", "18", "19", "20",
@@ -15,7 +17,23 @@ module ReturnsHelper
      "2026", "2027", "2028", "2029", "2030"]
   end
 
+  # 支払い処理の各画面URLを返す
   def return_payment_page_url(project_id, return_id, payment_view)
     "/projects/#{project_id}/returns/#{return_id}/#{payment_view}"
+  end
+
+  # 安心安全のセキュリティの画像URLを返す
+  def security_img_url
+    'https://readyfor.jp/assets/contribution/support02-63c9bd72490f296b6bb24ebbb94ceb79d315888c6bac3ef79e63cf9ef2c7e7bd.gif'
+  end
+
+  # お気軽にお問い合わせくださいの画像URLを返す
+  def inquiry_img_url
+    'https://readyfor.jp/assets/contribution/support03-875f72bd79d2db130a9a41aa35007461cc368c7fdec4d00c7133679b0d3552c2.gif'
+  end
+
+  # ご利用可能なクレジットカードリストの画像URLを返す
+  def creditcard_img_url
+    'https://readyfor.jp/assets/contribution/caed5-bd4673425aa4e9850a622c63dc220988f7e2e09ae4f84357b924f6040f483e4d.png'
   end
 end
