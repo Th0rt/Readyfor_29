b5b0f4a9e37b16b0db47a3f645282dc473e74805
関谷佑斗 <yhk3165t@gmail.com>
2018-07-19 16:20:45 +0900

    リターンの削除

--- app/controllers/returns_controller.rb
+++ app/controllers/returns_controller.rb
@@ -1,11 +1,17 @@
 class ReturnsController < ApplicationController
+  before_action :set_return, only: [:destroy]
+  before_action :set_project, only: [:new]
   def new
-    @project = Project.find(params[:project_id])
     @return = Return.new
   end
 
   def create
     Return.create(return_params)
+    redirect_to project_path(id: params[:project_id])
+  end
+
+  def destroy
+    @return.destroy
     redirect_to project_path(id: params[:project_id])
   end
 
@@ -18,5 +24,13 @@ class ReturnsController < ApplicationController
                                    :arrival_date,
                                    :returnimage
                                   ).merge(project_id: params[:project_id])
+  end
+
+  def set_return
+    @return = Return.find(params[:id])
+  end
+
+  def set_project
+    @project = Project.find(params[:project_id])
   end
 end
