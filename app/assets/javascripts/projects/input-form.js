ca6c4c57724543d376c3cd6bcadfba013d1029ec
kanji <nagao@nagaokanjinoMacBook-puro.local>
2018-07-20 13:00:55 +0900

    returnのnewにも適用

--- app/assets/javascripts/projects/input-form.js
+++ app/assets/javascripts/projects/input-form.js
@@ -1,6 +1,6 @@
 $(document).on('turbolinks:load', function () {
   // 目標金額を9桁までしか入力できないようにする
-  $('.goal-input').on('keypress', function () {
+  $('.price-input').on('keypress', function () {
     if (this.value.length > 8) {
       this.value = this.value.slice(0, 8);
     }
