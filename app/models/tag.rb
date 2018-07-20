e29663a094f852eb1836c810b6dbf94b3d01c4d1
Th0rt <yamaxanado@gmail.com>
2018-07-20 16:58:56 +0900

    リレーション削除設定

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,6 @@
 class Tag < ApplicationRecord
   validates :name, presence: true
 
-  has_many :tag_projects
+  has_many :tag_projects, dependent: :destroy
   has_many :projects, through: :tag_projects
 end
