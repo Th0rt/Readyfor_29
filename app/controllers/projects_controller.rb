431a56138d96b805a06725d5e213430b6088896f
関谷佑斗 <yhk3165t@gmail.com>
2018-07-20 16:27:22 +0900

    projectのリターンのビューを価格低い順に設定

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -11,7 +11,7 @@ class ProjectsController < ApplicationController
     view_history.delete_if { |id| id = @project.id }
     view_history << @project.id
     cookie_save("project_view_history", view_history)
-    @returns = @project.returns
+    @returns = @project.returns.order('price ASC' )
   end
 
   def new
