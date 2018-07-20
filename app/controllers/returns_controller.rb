0e59fdc6d3b4de13dd9352f416b2bb4cf7062603
関谷佑斗 <yhk3165t@gmail.com>
2018-07-20 15:06:41 +0900

    リターンコントローラの編集(return削除して1個もなければeditページに戻ってもらう)

--- app/controllers/returns_controller.rb
+++ app/controllers/returns_controller.rb
@@ -1,6 +1,6 @@
 class ReturnsController < ApplicationController
   before_action :set_return, only: [:destroy]
-  before_action :set_project, only: [:new]
+  before_action :set_project, only: [:new, :destroy]
   def new
     @return = Return.new
   end
@@ -12,7 +12,12 @@ class ReturnsController < ApplicationController
 
   def destroy
     @return.destroy
-    redirect_to project_path(id: params[:project_id])
+    @returns = @project.returns
+    if @returns.present?
+      redirect_to root_path
+    else
+      redirect_to edit_project_path(id: params[:project_id])
+    end
   end
 
   private
