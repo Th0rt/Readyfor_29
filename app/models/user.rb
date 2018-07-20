e1c5cce9d8c68b0bc62e983b98f8baf91c205687
kanji <nagao@nagaokaBookpuro.lan>
2018-07-20 09:45:14 +0900

    多対多の関係定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,6 @@ class User < ApplicationRecord
   validates :nickname, presence: true, length: { maximum: 10 }
 
   has_many :projects, dependent: :destroy
+  has_many :returns, through: :user_returns
   mount_uploader :avatar, AvatarUploader
 end
