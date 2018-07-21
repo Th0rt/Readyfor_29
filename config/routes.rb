d7b7f31e32a738d3c711449cb1a71e6976f384ea
kanji <nagao@nagaokaBookpuro.lan>
2018-07-21 12:13:12 +0900

    Revert "a"
    This reverts commit 11dad21f056fc41418cdb017a10424e5555ee3e1.

--- config/routes.rb
+++ config/routes.rb
@@ -19,13 +19,9 @@ Rails.application.routes.draw do
 
   resources :projects do
     resources :returns, only: [:new, :create]
-    # get 'returns/:id/payment1', to: 'returns#payment1'
-    # post 'returns/:id/payment2', to: 'returns#payment2'
-    # post 'returns/:id/payment3', to: 'returns#payment3'
-    # post 'returns/:id/payment4', to: 'returns#payment4'
-    get 'returns/payment1', to: 'returns#payment1'
-    get 'returns/payment2', to: 'returns#payment2'
-    get 'returns/payment3', to: 'returns#payment3'
-    get 'returns/payment4', to: 'returns#payment4'
+    get 'returns/:id/payment1', to: 'returns#payment1'
+    post 'returns/:id/payment2', to: 'returns#payment2'
+    post 'returns/:id/payment3', to: 'returns#payment3'
+    post 'returns/:id/payment4', to: 'returns#payment4'
   end
 end
