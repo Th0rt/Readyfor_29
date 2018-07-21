ad47e47d46f19b3938ea2c7b9bf716524281e698
tsukasa yamada <yamaxanado@gmail.com>
2018-07-21 12:48:08 +0900

    [fix] Resolve schema conflict

--- db/schema.rb
+++ db/schema.rb
@@ -50,7 +50,6 @@ ActiveRecord::Schema.define(version: 2018_07_19_164619) do
     t.index ["project_id"], name: "index_returns_on_project_id"
   end
 
-<<<<<<< HEAD
   create_table "tag_projects", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "project_id"
     t.bigint "tag_id"
@@ -66,7 +65,8 @@ ActiveRecord::Schema.define(version: 2018_07_19_164619) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["type"], name: "index_tags_on_type"
-=======
+  end
+
   create_table "user_returns", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "count", null: false
     t.bigint "return_id"
@@ -75,7 +75,6 @@ ActiveRecord::Schema.define(version: 2018_07_19_164619) do
     t.datetime "updated_at", null: false
     t.index ["return_id"], name: "index_user_returns_on_return_id"
     t.index ["user_id"], name: "index_user_returns_on_user_id"
->>>>>>> プロジェクト作成時にリターンも新規登録
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -100,11 +99,8 @@ ActiveRecord::Schema.define(version: 2018_07_19_164619) do
 
   add_foreign_key "projects", "users"
   add_foreign_key "returns", "projects"
-<<<<<<< HEAD
   add_foreign_key "tag_projects", "projects"
   add_foreign_key "tag_projects", "tags"
-=======
   add_foreign_key "user_returns", "returns"
   add_foreign_key "user_returns", "users"
->>>>>>> プロジェクト作成時にリターンも新規登録
 end
