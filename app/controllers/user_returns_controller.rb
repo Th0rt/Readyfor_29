dbaf09aa45ba4fd6bf3fa6bdbbae724a1bf2e6ac
kanji <nagao.rami@gmail.com>
2018-07-23 08:16:17 +0900

    購入履歴保存のcreateアクション作成

--- app/controllers/user_returns_controller.rb
+++ app/controllers/user_returns_controller.rb
@@ -1,10 +1,14 @@
 class UserReturnsController < ApplicationController
   def create
+    if UserReturn.create(user_return_params)
+      render "returns/done"
+    end
   end
 
   private
   def user_return_params
-    params.require(:user_return).permit(:count,
-                                  ).merge(return_id: params[:return_id], user_id: current_user.id)
+    params.require(:user_return).permit(
+      addresses_attributes: [:count, return_id: params[:return_id],
+      user_id: current_user.id])
   end
 end
