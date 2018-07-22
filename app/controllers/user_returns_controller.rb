7e6a889ed51166628316b4a27f03d5e509be4f41
kanji <nagao.rami@gmail.com>
2018-07-23 01:44:05 +0900

    createアクション追加

--- app/controllers/user_returns_controller.rb
+++ app/controllers/user_returns_controller.rb
@@ -1,2 +1,10 @@
 class UserReturnsController < ApplicationController
+  def create
+  end
+
+  private
+  def user_return_params
+    params.require(:user_return).permit(:count,
+                                  ).merge(return_id: params[:return_id], user_id: current_user.id)
+  end
 end
