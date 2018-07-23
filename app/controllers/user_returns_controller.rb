59a89fa0db503c49068df36bfbc56fcd3077dbcc
kanji <nagao.rami@gmail.com>
2018-07-23 17:14:19 +0900

    購入検証保存

--- app/controllers/user_returns_controller.rb
+++ app/controllers/user_returns_controller.rb
@@ -1,14 +1,15 @@
 class UserReturnsController < ApplicationController
   def create
+    @return = Return.find(params[:return_id])
+    @project = @return.project
     if UserReturn.create(user_return_params)
       render "returns/done"
     end
+    @project.update(total_support: @project.total_support += 1000)
   end
 
   private
   def user_return_params
-    params.require(:user_return).permit(
-      addresses_attributes: [:count, return_id: params[:return_id],
-      user_id: current_user.id])
+    params.require(:user_return).permit(:count).merge(return_id: params[:return_id], user_id: current_user.id)
   end
 end
--- app/models/return.rb
+++ app/models/return.rb
@@ -2,6 +2,5 @@ class Return < ApplicationRecord
   belongs_to :project
   has_many :user_returns
   has_many :users, through: :user_returns
-  accepts_nested_attributes_for: user_returns
   mount_uploader :returnimage, ReturnImageUploader
 end
