3b1fed82bb28836b2ccd5cfd416cd51e0b80e678
関谷佑斗 <yhk3165t@gmail.com>
2018-07-24 17:02:40 +0900

    jsの修正

--- app/assets/javascripts/projects/new-image.js
+++ app/assets/javascripts/projects/new-image.js
@@ -1,9 +1,11 @@
 $(document).on('turbolinks:load', function () {
   $('#project-image').change(function () {
-    var file = this.querySelector('input[type=file]').files[0];
+    $('#project-img-default').remove();
+    console.log(this)
+    var file = $(this).prop('files')[0];
     var reader = new FileReader();
     reader.onload = function () {
-      var img_src = $('#project-img-show').attr('src', reader.result);
+      var img_src = $('<img>').attr('src', reader.result);
       $('#project-img-show').html(img_src);
     }
     reader.readAsDataURL(file);
