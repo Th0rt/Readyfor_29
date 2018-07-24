72d945051e14b20a40f836248089e5057d43df4d
Saoki11111 <t.aoki11111@gmail.com>
2018-07-24 19:11:54 +0900

    [fix]いいねのカウント数が期待通り表示されるように修正した。

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,25 +1,18 @@
 class LikesController < ApplicationController
 
-  before_action :set_project, only: [:create, :destroy]
-
   def create
     @like = Like.create(user_id: current_user.id, project_id: params[:project_id])
     @likes = Like.where(project_id: params[:project_id])
-    # @like = current_user.likes.create(project_id: params[:project_id])
     @projects = Project.all
+    @project = Project.find(params[:project_id])
   end
 
   def destroy
     like = Like.find_by(user_id: current_user.id, project_id: params[:project_id])
-    # like = current_user.likes.find_by(project_id: params[:project_id])
     like.destroy
     @likes = Like.where(project_id: params[:project_id])
     @projects = Project.all
+    @project = Project.find(params[:project_id])
   end
 
-  private
-
-  def set_project
-   @project = Project.find(params[:project_id])
-  end
 end
