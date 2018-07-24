71a4a54eff14775a2e19ab2d1b2861297e51ccf7
kanji <nagao.rami@gmail.com>
2018-07-24 14:12:34 +0900

    各リターンの複数レコード一択登録

--- app/controllers/user_returns_controller.rb
+++ app/controllers/user_returns_controller.rb
@@ -2,21 +2,24 @@ class UserReturnsController < ApplicationController
   def create
     @return = Return.find(params[:return_id])
     @project = @return.project
-    if UserReturn.create(user_return_params)
-      render "returns/done"
-    end
 
-    # 支援総額への加算
+    user_returns = []
     sum = 0
-    params[:user_return][:number].each do |key, value|
+    user_return_params[:number].each do |key, value|
+      user_returns << UserReturn.new(count: value.to_i, return_id: key.to_i, user_id: user_return_params[:user_id])
       return_item = Return.find(key)
       sum += return_item.price * value.to_i
     end
-    @project.update(total_support: @project.total_support += sum)
+
+    if UserReturn.import user_returns
+      if @project.update(total_support: @project.total_support += sum)
+        render 'returns/done'
+      end
+    end
   end
 
   private
   def user_return_params
-    params.require(:user_return).permit(:count).merge(return_id: params[:return_id], user_id: current_user.id)
+    {number: params[:user_return][:number], return_id: params[:return_id], user_id: current_user.id}
   end
 end
