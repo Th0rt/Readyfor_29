38ca08dba6b4cb5ed6b92913ac845911b08edafa
kanji <nagao.rami@gmail.com>
2018-07-25 12:19:45 +0900

    複雑処理のメソット化

--- app/controllers/user_returns_controller.rb
+++ app/controllers/user_returns_controller.rb
@@ -3,18 +3,8 @@ class UserReturnsController < ApplicationController
     @return = Return.find(params[:return_id])
     @project = @return.project
 
-    user_returns = []
-    sum = 0
-    user_return_params[:number].each do |key, value|
-      unless value.to_i == 0
-        user_returns << UserReturn.new(count: value.to_i, return_id: key.to_i, user_id: user_return_params[:user_id])
-        return_item = Return.find(key)
-        sum += return_item.price * value.to_i
-      end
-    end
-
-    if UserReturn.import user_returns
-      if @project.update(total_support: @project.total_support += sum)
+    if UserReturn.import UserReturn.user_return_array(user_return_params[:number], user_return_params[:user_id])
+      if @project.update(total_support: @project.total_support += UserReturn.return_sum(user_return_params[:number]))
         render 'returns/done'
       end
     end
--- app/models/user_return.rb
+++ app/models/user_return.rb
@@ -1,4 +1,25 @@
 class UserReturn < ApplicationRecord
   belongs_to :user
   belongs_to :return
+
+  def self.user_return_array(params_number, params_user_id)
+    user_returns = []
+    params_number.each do |key, value|
+      unless value.to_i == 0
+        user_returns << UserReturn.new(count: value.to_i, return_id: key.to_i, user_id: params_user_id)
+      end
+    end
+    return user_returns
+  end
+
+  def self.return_sum(params_number)
+    sum = 0
+    params_number.each do |key, value|
+      unless value.to_i == 0
+        return_item = Return.find(key)
+        sum += return_item.price * value.to_i
+      end
+    end
+    return sum
+  end
 end
