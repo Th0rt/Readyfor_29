8905f9d64e9e4d50481d3676c19d22f7b21f1970
kanji <nagao.rami@gmail.com>
2018-07-26 15:44:10 +0900

    支援人数の加算

--- app/controllers/user_returns_controller.rb
+++ app/controllers/user_returns_controller.rb
@@ -6,7 +6,9 @@ class UserReturnsController < ApplicationController
     # リターン購入分のレコード保存とプロジェクトのtotal_supportに購入金額分を加算する
     if UserReturn.import UserReturn.user_return_array(user_return_params[:number], user_return_params[:user_id])
       if @project.update(total_support: @project.total_support += Project.return_sum(user_return_params[:number]))
-        render 'returns/done'
+        if Return.total_user_sum(user_return_params[:number], current_user.id)
+          render 'returns/done'
+        end
       end
     end
   end
--- app/models/return.rb
+++ app/models/return.rb
@@ -3,4 +3,17 @@ class Return < ApplicationRecord
   has_many :user_returns, dependent: :destroy
   has_many :users, through: :user_returns
   mount_uploader :returnimage, ReturnImageUploader
+
+  def self.total_user_sum(params_number, current_user_id)
+    params_number.each do |key, value|
+      unless value.to_i == 0
+        return_item = Return.find(key)
+        user_returns = UserReturn.where("(return_id = ?) AND (user_id = ?)", return_item.id, current_user_id)
+        return_item.update(total_user: return_item.total_user += 1) unless user_returns.count == 0
+
+        return true;
+      end
+    end
+    return false;
+  end
 end
