921aed98f59c1be2e6ff117b930b594f82d76925
kanji <nagao.rami@gmail.com>
2018-07-26 12:11:03 +0900

    ログインしていない場合ログインページに飛ぶ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,6 +15,8 @@ class ApplicationController < ActionController::Base
   end
 
   def require_login
-    redirect_to new_user_session_path
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
   end
 end
