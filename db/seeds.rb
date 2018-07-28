5ee783ca43a09c8925677ab2fc23c34ad87428c6
Th0rt <yamaxanado@gmail.com>
2018-07-28 15:26:28 +0900

    初期地域タグの追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,3 +39,28 @@ initial_tag_lists = [
   }
 ]
 
+initial_tag_lists.each do |tag_list|
+  category = Category.find_by(name: tag_list[:category_name])
+  tag_list[:tag_name].each do |tag_name|
+    tag = category.tags.create(name: tag_name, type: '')
+  end
+end
+
+# 地域
+intial_region = %w(
+  北海道 札幌 旭川 知床
+  東北 青森県 岩手県 宮城県 秋田県 山形県 福島県
+  関東 茨城県 栃木県 群馬県 埼玉県 千葉県 東京都 神奈川県
+  中部 新潟県 富山県 石川県 福井県 山梨県 長野県 岐阜県 静岡県 愛知県
+  近畿 三重県 滋賀県 京都府 大阪府 兵庫県 奈良県 和歌山県
+  中国 鳥取県 島根県 岡山県 広島県 山口県
+  四国 徳島県 香川県 愛媛県 高知県
+  九州 福岡県 佐賀県 長崎県 熊本県 大分県 宮崎県 鹿児島県
+  沖縄
+  海外 アジア オセアニア 北米・中米 中近東 ヨーロッパ 南米 アフリカ
+)
+
+intial_region.each do |region|
+  Region.create(name: region)
+end
+
