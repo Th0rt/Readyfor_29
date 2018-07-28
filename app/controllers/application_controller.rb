5fb5e5a077a3a9b82d0fbf10451e2cdf6e424324
Saoki11111 <t.aoki11111@gmail.com>
2018-07-28 21:09:38 +0900

    [fix]不要な改行の削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,6 @@ class ApplicationController < ActionController::Base
     render template: 'errors/error_500', status: 500, layout: 'application', content_type: 'text/html'
   end
 
-
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
   end
