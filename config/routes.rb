969fdeb79e111a0f05bfdb485ad36bb356d96739
Saoki11111 <t.aoki11111@gmail.com>
2018-07-28 21:09:51 +0900

    [fix]不要な空行の削除

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,6 @@ Rails.application.routes.draw do
   get 'toppage/art',        to: 'toppage#art'
   get 'toppage/challenge',  to: 'toppage#challenge'
 
-
   resources :tags, only: [:index, :show, :new, :create, :update, :destroy]
   resources :region, only: [:create, :update, :destroy]
 
