792703bb47f995cc07b011b360df2d735c9aa38b
kanji <nagao.rami@gmail.com>
2018-07-28 20:26:35 +0900

    数量を選択した、リターン価格をリストに追加

--- app/assets/javascripts/returns/price-sum.js
+++ app/assets/javascripts/returns/price-sum.js
@@ -1,4 +1,13 @@
 $(document).on('turbolinks:load', function () {
+  function buildHTML(return_item, number) {
+    var html = `<p>
+                  <span>
+                    ${return_item.price} × ${number}個
+                  </span>
+                </p>`;
+    return html;
+  }
+
   $('.number-select').change(function () {
     // リターンのIDを取得
     var return_id_str = $(this).attr("id");
@@ -20,11 +29,18 @@ $(document).on('turbolinks:load', function () {
     })
       .done(function (returns) {
         if (returns.length !== 0) {
-          // 全リターンのoptionオブジェクト取得
+          $('#return-price-list').text("");
+
           $('.number-select').each(function (index) {
-            console.log($(this).val());
-            sum += returns[index].price * parseInt($(this).val(), 10);
+            //リターンの数量
+            var number = parseInt($(this).val(), 10);
+            if (number != 0) {
+              var html = buildHTML(returns[index], number);
+              $('#return-price-list').append(html);
+              sum += returns[index].price * number;
+            }
           });
+
           // 合計金額表示
           $('#return-sum').text(`${sum}円`);
         }
