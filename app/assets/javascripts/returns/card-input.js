33e17cc2743dae599a7654941bfc27796813c1d2
kanji <nagao.rami@gmail.com>
2018-07-29 15:18:53 +0900

    入力文字数制限

--- app/assets/javascripts/returns/card-input.js
+++ app/assets/javascripts/returns/card-input.js
@@ -10,4 +10,14 @@ $(document).on('turbolinks:load', function () {
       $("#card-input").submit();
     }
   });
+  $('#card-input-form').on('keypress', function () {
+    if (this.value.length > 15) {
+      this.value = this.value.slice(0, 15);
+    }
+  });
+  $('#cvc-input-form').on('keypress', function () {
+    if (this.value.length > 2) {
+      this.value = this.value.slice(0, 2);
+    }
+  });
 });
