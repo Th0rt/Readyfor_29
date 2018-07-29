10358850e7a4ef05f7c6db74d7b4d11da3ddddee
kanji <nagao.rami@gmail.com>
2018-07-29 15:46:42 +0900

    銀行振込の入力制限実装

--- app/assets/javascripts/returns/bank-input.js
+++ app/assets/javascripts/returns/bank-input.js
@@ -1,19 +1,27 @@
 $(document).on('turbolinks:load', function () {
   $("#bank-input").submit(function () {
-    if ($("bank_name-form").val() == '') {
+    if ($("#bank_name-form").val() == '') {
       alert('銀行名を入力してください');
       return false;
-    } else if ($("branch_office-form").val() == '') {
+    } else if ($("#branch_office-form").val() == '') {
       alert('支店名を入力してください');
       return false;
-    } else if ($("name-form").val() == '') {
+    } else if ($("#name-form").val() == '') {
       alert('口座氏名を入力してください');
       return false;
-    } else if ($("account_number-form").val() == '') {
+    } else if ($("#account_number-form").val() == '') {
       alert('口座番号を入力してください');
+      return false;
+    } else if ($("#account_number-form").val().length < 7) {
+      alert('口座番号が7桁入力されていません');
       return false;
     } else {
       $("#bank-input").submit();
+    }
+  });
+  $('#account_number-form').on('keypress', function () {
+    if (this.value.length > 6) {
+      this.value = this.value.slice(0, 6);
     }
   });
 });
