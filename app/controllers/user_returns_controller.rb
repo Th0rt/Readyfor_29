2077b97b9a5f8ad8990b4c46d6b025a2465f34ab
kanji <nagao.rami@gmail.com>
2018-07-31 02:22:50 +0900

    保存処理比較条件変更

--- app/controllers/user_returns_controller.rb
+++ app/controllers/user_returns_controller.rb
@@ -5,15 +5,18 @@ class UserReturnsController < ApplicationController
 
     return_sum = Project.return_sum(user_return_params[:number])
     # 決済処理
-    Payjp::Charge.create(currency: 'jpy', amount: return_sum, card: params['payjpToken'])
-
-    # 初めてリターン購入した人ならばtotal_userに1足す、リターン購入分のレコード保存、プロジェクトのtotal_supportに購入金額分を加算
-    if Return.total_user_sum(user_return_params[:number], current_user.id)
-      if UserReturn.import UserReturn.user_return_array(user_return_params[:number], user_return_params[:user_id])
-        if @project.update(total_support: @project.total_support += return_sum)
-            render 'returns/done'
+    if !params['payjpToken'] == '' && Payjp::Charge.create(currency: 'jpy', amount: return_sum, card: params['payjpToken'])
+      # 初めてリターン購入した人ならばtotal_userに1足す、リターン購入分のレコード保存、プロジェクトのtotal_supportに購入金額分を加算
+      if Return.total_user_sum(user_return_params[:number], current_user.id)
+        if UserReturn.import UserReturn.user_return_array(user_return_params[:number], user_return_params[:user_id])
+          if @project.update(total_support: @project.total_support += return_sum)
+              render 'returns/done'
+          end
         end
       end
+    else
+      flash[:alert] = '決済処理に失敗しました。もう一度やり直してください。'
+      redirect_to project_path(params[:project_id])
     end
   end
 
