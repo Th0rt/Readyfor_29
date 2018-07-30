a0a845dd47f20e665b803965fcdb3f3cbf727314
kanji <nagao.rami@gmail.com>
2018-07-31 01:41:20 +0900

    決済トークン発行jsの作成

--- app/assets/javascripts/returns/payjp.js
+++ app/assets/javascripts/returns/payjp.js
@@ -1,36 +1,35 @@
 $(document).on('turbolinks:load', function () {
-  var form = $("#new_user_return"),
-    card_number = form.find('#card-value'),
-    cvc = form.find('#cvc-value'),
-    month = form.find('#month-value'),
-    year = form.find('#year-value');
+  var form = $("#new_user_return");
+  var number = $('#card-value');
+  var cvc = $('#cvc-value');
+  var exp_month = $('#month-value');
+  var exp_year = $('#year-value');
 
-  $(form).on('submit', function (e) {
+  form.on('submit', function (e) {
     e.preventDefault();
 
-    form.find("#new_user_return").prop("disabled", true);
+    // form.prop("disabled", true);
 
-    var card = {
-      card_number: card_number.value,
-      cvc: cvc.value,
-      month: month.value,
-      year: year.value
+    var card_info = {
+      number: parseInt(number.val(), 10),
+      cvc: parseInt(cvc.val(), 10),
+      exp_month: parseInt(exp_month.val(), 10),
+      exp_year: parseInt(exp_year.val(), 10)
     };
     Payjp.setPublicKey('pk_test_0056da6ac88997ee48c98ac8');
-    Payjp.createToken(card, function (s, response) {
+    Payjp.createToken(card_info, function (s, response) {
       if (response.error) {
         // form.find('.payment-errors').text(response.error.message);
-        form.find('button').prop('disabled', false);
+        form.prop("disabled", false);
         alert('決済エラーが発生しました。もう一度やり直してください。');
       }
       else {
-
         var token = response.id;
 
         form.append($('<input type="hidden" name="payjpToken" />').val(token));
 
         console.log(token);
-        //form.get(0).submit();
+        form.get(0).submit();
       }
     });
   });
