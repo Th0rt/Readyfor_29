5530fd57cfe06e02697f737e9b25f109f35858f0
kanji <nagao.rami@gmail.com>
2018-08-01 15:11:35 +0900

    エディタの機能制限

---
+++ app/assets/javascripts/ckeditor/config.js
@@ -0,0 +1,10 @@
+CKEDITOR.editorConfig = function (config) {
+  config.toolbar_mini = [
+    ["Link", "Image", "Table", "HorizontalRule"],
+    ["Bold", "Underline", "Strike"],
+    ["FontSize", "TextColor"],
+    ["code"],
+    ["Recommend"],
+    ["document"]
+  ];
+}
